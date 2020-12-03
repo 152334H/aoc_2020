9bb4d41d05c1adcff0fe43b9c346cfe19745e20c
152334H <54623771+152334H@users.noreply.github.com>
2020-12-03 15:33:44 +0800

    working sloc reduction

--- 2020/3/solve.py
+++ 2020/3/solve.py
@@ -1,5 +1,5 @@
 from aoc import *
 grid = makeGrid(s:=sread('input', str, '\n'), xmax:=len(s[0]), ymax:=len(s))
-def traverse(R,D): sum(grid[((y*R//D)%xmax,y)]=='#' for y in range(0,ymax,D))
+traverse = lambda R,D: sum(grid[((y*R//D)%xmax,y)]=='#' for y in range(0,ymax,D))
 print(traverse(3,1))
 print(prod(traverse(*t) for t in [(r,1) for r in range(1,8,2)]+[(1,2)]))
